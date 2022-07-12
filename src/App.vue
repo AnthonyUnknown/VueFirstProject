a78e4c3e88f79b7d22e893d9a99d91f6346de304
Anthony <antony-romanovskiyy@mail.ru>
2022-07-12 22:22:21 +0300

    Vue. UI. Add project. Make add posts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<form >
+    <input v-bind:value="name" @input="changeName" type="text" placeholder="Имя">
+    <input v-bind:value="title" @input="changeTitle" type="text" placeholder="Описание">
+    <button type="submit" @click="createPost">Создать пост</button>
+</form>
+<div class="post" v-for="post in posts">
+<div>{{post.name}}</div>
+<div>{{post.title}}</div>
+</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+    data () {
+        return {
+            posts: [
+        {id: 1, name: "Пост1", title: "Описание поста 1"},
+        {id: 2, name: "Пост2", title: "Описание поста 2"},
+        {id: 3, name: "Пост3", title: "Описание поста 3"}
+       ],
+       name: "",
+       title: "",
+        }
+    },
+    methods: {
+       changeName (event) {
+        this.name = event.target.value
+       },
+       changeTitle (event) {
+        this.title = event.target.value
+       },
+       createPost (event) {
+        event.preventDefault()
+        const newPost = {
+            id: Date.now,
+            name: this.name,
+            title: this.title
+        }
+        this.posts = [...this.posts, newPost]
+        this.name = ""
+        this.title = ""
+       }
+    }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+        .post {
+            border: 1px solid blue;
+            padding: 20px;
+            margin-top: 20px;
+        }
 </style>
